489c062c9aa89f3d4eff944999e84cd050eace9b
Christian Rackerseder <git@echooff.de>
2026-03-07 17:51:37 +0100

    Remove deprecated rule

--- configs/base.js
+++ configs/base.js
@@ -344,7 +344,6 @@ export const baseConfig = {
         ],
         'eslint-comments/no-aggregating-enable': 'error',
         'eslint-comments/no-duplicate-disable': 'error',
-        'eslint-comments/no-unused-disable': 'error',
         'eslint-comments/no-unused-enable': 'error',
         'eslint-comments/no-restricted-disable': 'off',
         'eslint-comments/no-unlimited-disable': 'error',
